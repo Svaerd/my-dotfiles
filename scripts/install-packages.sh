b27984da8f5f1eb5a6fa0052694f9c6fcc4f9100
Svaerd <https://husam.danish@proton.me/>
2025-04-30 18:53:51 +0800

    new pkgs on script

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 packages=(
+    7zip
     bat
+    bluez
+    bluez-utils
+    brightnessctl
+    btop
     cmake
+    cliphist
     fastfetch
     fish
+    fd
+    flatpak
+    fzf
     gtrash-bin
     git
     ghostscript
@@ -19,18 +28,24 @@ packages=(
     hyprpaper
     hyprshade
     hyprutils
+    hyprpolkitagent
     imagemagick
     kitty
     kdeconnect
     lazygit
     lua
+    nautilus
     neovim
+    noto-fonts-emoji
     npm
     nwg-look
     pacseek
+    ripgrep
     rofi-calc
     rofi-emoji
     rofi-wayland
+    ttf-firacode-nerd
+    timeshift
     vivaldi
     waypaper
     wl-clipboard
@@ -43,4 +58,4 @@ for package in "${packages[@]}"; do
     yay -S --noconfirm "${package}"
 done
 
-xargs flatpak install -y <flatpaks.txt
+#xargs flatpak install -y <flatpaks.txt
