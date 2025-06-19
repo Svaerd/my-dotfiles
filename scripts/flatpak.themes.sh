fb15739c1af38f9da15106439b1e0f5405cda533
Svaerd <husam.danish@proton.me>
2025-06-19 18:12:13 +0800

    minor update

--- scripts/flatpak.themes.sh
+++ scripts/flatpak.themes.sh
@@ -1,14 +1,13 @@
 override=(
-    --filesystem=~/.themes
-    --filesystem=~/.local/share/themes
-    --filesystem=~/.icons
-    --filesystem=~/.local/share/icons
-    --filesystem=~/.fonts
-    --filesystem=~/.local/share/fonts
-    --user --filesystem=xdg-config/gtk-4.0
-    --filesystem=xdg-config/gtk-4.0
+	--filesystem=~/.themes
+	--filesystem=~/.local/share/themes
+	--filesystem=~/.icons
+	--filesystem=~/.local/share/icons
+	--filesystem=~/.fonts
+	--filesystem=~/.local/share/fonts
+	--filesystem=~/.config/gtk-4.0
 )
 
 for flatpak in "${override[@]}"; do
-    sudo flatpak override "$flatpak"
+	sudo flatpak override "$flatpak"
 done
