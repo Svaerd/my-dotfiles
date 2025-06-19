67ca52dbd61c7cbec9251c8f810e51da4370d77d
Svaerd <husam.danish@proton.me>
2025-06-19 18:13:28 +0800

    LSP server to use python, in short plugin for pythom

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,9 @@ local opt = vim.opt
 opt.tabstop = 4
 opt.wrap = true -- Disable/enable line wrap
 opt.shiftwidth = 2 -- Size of an indent
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
